294ec82fbf4c70c945045b146b591df163f8ea84
이민정 <coolmj1202@gmail.com>
2025-08-11 18:36:47 +0900

    reward 모델 수정 완료

--- reward/models.py
+++ reward/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class reward(models.Model):
+    name=models.CharField(max_lenth=100)
+    image=models.ImageField(upload_to='rewards/', blank=True, null=True)
+    price=models.IntegerField(default=0) # 교환에 필요한 포인트
+    created_at=models.DateTimeField(auto_now_add=True)
+    # 가중치는 추가 안했어용
