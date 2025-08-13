7e1ec39e8ffb022d313d59cbee6f36b10c372e60
이채영 <lohoho619@inha.edu>
2025-08-14 08:49:26 +0900

    place 등록/조회/단일 완료

--- place/views.py
+++ place/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from .models import Place
+from .serializers import PlaceSerializer
+
+from rest_framework.permissions import AllowAny   # 임시
+
+class PlaceListCreateView(generics.ListCreateAPIView):
+    queryset = Place.objects.all().order_by('-id')
+    serializer_class = PlaceSerializer
+    permission_classes = [AllowAny]   # 임시
+
+
+class PlaceDetailView(generics.RetrieveAPIView):
+    queryset = Place.objects.all()
+    serializer_class = PlaceSerializer
+    permission_classes = [AllowAny]   # 임시
