6d494c6cc99cd5ada0dd6e47e325c39ff6a24803
ko <daekklove@gmail.com>
2025-08-18 14:25:49 +0900

    [Feat] Create Summary app

---
+++ summary/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from user.models import User
+
+
+# Create your models here.
+
+class Summary(models.Model):
+    session = models.ForeignKey(User, on_delete=models.CASCADE, related_name="summaries")
+    summary_text = models.TextField()
+    sum_seq = models.IntegerField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return 
