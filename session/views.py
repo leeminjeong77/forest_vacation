7d94b1c781d6e3ee37e7bfd44f7304870e3ba4ba
ko <daekklove@gmail.com>
2025-08-21 11:34:34 +0900

    [Feat] session 생성 api

--- session/views.py
+++ session/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from openai import OpenAI
+
+from .. import config
+
+from django.shortcuts import render
+from django.conf import settings
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.hashers import check_password
+from .models import Session
+
+import uuid #고유한 이름 만들어줌
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
+
+from .serializers import SessionSerializer
+
+from urllib.parse import urlparse
 
 # Create your views here.
+
+client = OpenAI(
+	api_key = config.OPENAI_API_KEY
+)
+
+response = client.responses.create(
+  model="gpt-4o-mini",
+  input="write a haiku about ai",
+  store=True,
+)
+
+# @api_view(['GET'])
+# @permission_classes([AllowAny])
+# def get_preset_questions(request):
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def create_session(request):
+    if request.method == 'POST':
+        user_id = request.data.get("user_id")
+        serializer = SessionSerializer(data = {})
+        return 2
+    else:
+        return 1 
