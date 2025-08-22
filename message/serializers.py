093454727b5477df344b5407364ec1edcb596232
ko <daekklove@gmail.com>
2025-08-22 22:32:41 +0900

    [Fix] message 엔티티 수정

--- message/serializers.py
+++ message/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Message
 from django.contrib.auth.hashers import make_password
 
-class SummarySerializer(serializers.ModelSerializer):
+class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
-        fields = ['id', 'session_id', 'role', 'content', 'msg_seq', 'created_at', 'updated_at']
+        fields = ['id', 'session', 'role', 'content', 'msg_seq', 'created_at', 'updated_at']
